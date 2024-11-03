50dccb8c3864c3c4ed8a127b6e34eae4e0f992bc
mutkin251 <mutkin@kse.org.ua>
2024-11-03 18:16:32 +0200

    Improvements

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 
 def congratulate_user():
-    print(f"Congratulations, you won! your words: {guesses}")
+    print(f"Congratulations, you won! Your words: {guesses}")
 
 
 def is_game_over():
@@ -13,18 +13,24 @@ def is_game_over():
 def guess_is_valid(candidate):
     for letter in candidate:
         if letter not in word:
-            print(f"You can not use letter {letter}")
+            print(f"You cannot use letter '{letter}'")
             return False
         count = word.count(letter)
         if count < candidate.count(letter):
-            print(f"You can use letter {letter} only {count} times")
+            print(f"You can use letter '{letter}' only {count} times")
             return False
     return True
 
 
+def has_already_guessed(candidate):
+    if candidate in guesses:
+        print(f"You've already guessed the word '{candidate}'. Try again!")
+        return True
+    return False
+
+
 guessed = 0
 errors = 0
-
 guesses = []
 
 WORDS_TO_WIN = 5
@@ -34,12 +40,14 @@ words = words_fetcher.fetch_words(min_letters=9, max_letters=9)
 full_list = words_fetcher.fetch_words(min_letters=3, max_letters=9)
 word = words[random.randrange(0, len(words))]
 
-print(f"Can you make up {WORDS_TO_WIN} words from letters in word provided by me?")
+print(f"Can you make up {WORDS_TO_WIN} words from letters in the word provided by me?")
 print(f"Your word is '{word}'")
 
-
 while not is_game_over():
-    guess = input("Your next take: ")
+    guess = input("Your next guess: ")
+
+    if has_already_guessed(guess):  # Check for duplicates first
+        continue
 
     if not guess_is_valid(guess):
         continue
@@ -50,7 +58,7 @@ while not is_game_over():
         if guessed == WORDS_TO_WIN:
             congratulate_user()
             exit()
-        print(f"That's right! {WORDS_TO_WIN - guessed} to go")
+        print(f"That's right! {WORDS_TO_WIN - guessed} more to go")
     else:
         errors += 1
-        print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+        print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives left")
